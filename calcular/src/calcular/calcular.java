6a574f59e955a41063e731b5539356f1318b8ea4
gabrielnunesfalcao <150195672+gabrielnunesfalcao@users.noreply.github.com>
2023-11-08 19:42:40 -0300

    Update calcular.java

--- calcular/src/calcular/calcular.java
+++ calcular/src/calcular/calcular.java
@@ -5,37 +5,56 @@ public class calcular {
 	public static void main(String[] args) {
 		while(true) {			
 			Scanner input = new Scanner(System.in);
-			System.out.println("");
-			if (opcao == 1)
-			System.out.println("Digite o primeiro numero: ");
-			double soma1 = input.nextDouble();			
-			System.out.println("Digite o segundo numero: ");
-			double soma2 = input.nextDouble();
-			double resultado = soma1 + soma2;
-			System.out.println("Resultado: " +  resultado );
+			System.out.println("Escolha uma operação");
+			System.out.println("1 - Soma");
+			System.out.println("2 - Diminuir");
+			System.out.println("3 - Multiplicar");
+			System.out.println("4 - Divisão");
+			System.out.println("5 - Sair");
 			
-			else
-			System.out.println("Digite o primeiro numero: ");
-			double diminuir1 = input.nextDouble();			
-			System.out.println("Digite o segundo numero: ");
-			double diminuir2 = input.nextDouble();
-			double resultado = diminuir1 - diminuir2;
-			System.out.println("Resultado: " +  resultado );
+			int escolha = input.nextInt();
 			
+			if (escolha == 5) {
+				System.out.println("Encerrando a calculadora");
+				break;
+			}
 			
 			System.out.println("Digite o primeiro numero: ");
-			double mult1 = input.nextDouble();			
-			System.out.println("Digite o segundo numero: ");
-			double mult2 = input.nextDouble();
-			double resultado = mult1 * mult2;
-			System.out.println("Resultado: " +  resultado );
+			double numero1 = input.nextDouble();
 			
-			
-			System.out.println("Digite o primeiro numero: ");
-			double dividir1 = input.nextDouble();			
 			System.out.println("Digite o segundo numero: ");
-			double dividir2 = input.nextDouble();
-			double resultado = dividir1 / dividir2;
+			double numero2 = input.nextDouble();
+			
+			double resultado = 0.0;
+			
+			switch (escolha) {
+			case 1:
+				resultado = numero1 + numero2;
+				break;
+			case 2:
+				resultado = numero1 - numero2;
+				break;
+			case 3: 
+				resultado = numero1 * numero2;
+			case 4:
+				if (numero2 != 0) {
+				resultado = numero1 / numero2;
+				} else {
+					System.out.println("Erro: Divisão por zero!");
+					continue;
+					
+				}
+				break;
+			default:
+				System.out.println("Opção inválida. Tente novamente.");
+				continue;
+			}
+			
+			System.out.println("Resultado: " + resultado);
+				}
+	
+}
+}
 			System.out.println("Resultado: " +  resultado );
 		
 		
